303ce1d6d84db983c5f3399541fe2556750201ec
jmschindele <45886434+jmschindele@users.noreply.github.com>
2019-02-25 17:20:00 -0500

    added prototype to HTML

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+var startGame;
+var wordOptions = ["dog", "cat", "pig", "frog", "moose", "llama", "jelly"]
+var userGuess;
+var userScore;
+var userLife;
+
+
+// alert("Press any key to start!")
+
+document.getElementById("remaining-guesses").onkeyup = function (event) {
+    console.log(a);
+}
