3f51ffacbead8446f39d9f71ed95d85621f9a880
jmschindele <45886434+jmschindele@users.noreply.github.com>
2019-02-26 00:29:29 -0500

    key logger and random word selector added to JS. placeholder image in css. html skeleton layed out.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,40 @@
 var startGame;
-var wordOptions = ["dog", "cat", "pig", "frog", "moose", "llama", "jelly"]
+var alphabet = [];
+var wordOptions = ["dog", "cat", "pig", "frog", "moose", "llama", "jelly"];
+var word;
 var userGuess;
 var userScore;
 var userLife;
+var userInput = document.getElementById("user-input");
 
 
-// alert("Press any key to start!")
+// for (i=0, i<randomWord.length, i++); {
 
-document.getElementById("remaining-guesses").onkeyup = function (event) {
-    console.log(a);
+// }
+
+function wordGen() {
+  
+  word = (Math.floor(Math.random() * wordOptions.length));
 }
+
+wordGen();
+
+document.getElementById("unsolved-puzzle").textContent = (wordOptions[word])
+
+document.onkeyup = function(event) {
+  userGuess = event.key;
+  userInput.textContent = userGuess;
+  alphabet.push(userGuess);
+  document.getElementById("guessed-letters").innerHTML = alphabet.join(" ");
+console.log(alphabet);
+};
+
+
+
+
+
+// alphabet.splice(userGuess, userGuess);
+
+
+// document.getElementById("guessed-letters").innerHTML = alphabet;
+// console.log(alphabet);
