952a2d8e5e2d4c9212a36ee50ffbf6e0cb3ebdc3
jmschindele <45886434+jmschindele@users.noreply.github.com>
2019-03-01 22:27:15 -0500

    changed javascript completely. Game functions.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,40 +1,77 @@
-var startGame;
-var alphabet = [];
-var wordOptions = ["dog", "cat", "pig", "frog", "moose", "llama", "jelly"];
-var word;
+// create an array of words for the computer to guess
+var targetWord = ["apple", "banana", "orange", "grape", "strawberry"];
+// define other variables
+var start;
+var wordIndex = 0;
+var computerChoice = "";
+var letterBlanks = "";
+var blankHolder = [];
 var userGuess;
-var userScore;
-var userLife;
+var keyPress;
 var userInput = document.getElementById("user-input");
+var allBlanks = [];
+var wrongGuess = [];
+var wins = 0;
 
-
-// for (i=0, i<randomWord.length, i++); {
-
-// }
+//Set lives to 6 and add to page
+var lives = 6;
+document.getElementById("lives").innerHTML = lives;
+// computer guesses a word
 
 function wordGen() {
-  
-  word = (Math.floor(Math.random() * wordOptions.length));
+  wordIndex = Math.floor(Math.random() * targetWord.length);
+}
+wordGen();
+computerChoice = targetWord[wordIndex];
+
+// computer converts word into underscores and displays on wedpage
+
+var wordBlanks = document.createElement("span");
+
+for (var i = 0; i < computerChoice.length; i++) {
+  allBlanks.push("_");
+  wordBlanks.textContent = allBlanks.join(" ");
+  document.getElementById("unsolved-puzzle").appendChild(wordBlanks);
 }
 
-wordGen();
+// user inputs a key to guess word
+document.onkeypress = function(e) {
+  keyPress = event.key;
 
-document.getElementById("unsolved-puzzle").textContent = (wordOptions[word])
+ //Add condition that prompts user of repeated keys 
+  if (wrongGuess.indexOf(keyPress) > -1) {
+    alert("You've already pressed that letter!");
+  }
 
-document.onkeyup = function(event) {
-  userGuess = event.key;
-  userInput.textContent = userGuess;
-  alphabet.push(userGuess);
-  document.getElementById("guessed-letters").innerHTML = alphabet.join(" ");
-console.log(alphabet);
+//Add win condition
+  if (computerChoice == allBlanks.join("").toString()) {
+    alert("You Win!");
+    location.reload();
+    win++;
+  }
+//Add gameover condition
+  if (lives === 0) {
+    alert("You Lose!");
+    location.reload();
+  }
+//Add a for loop to replace blanks with characters based on position
+  for (var i = 0; i < computerChoice.length; i++) {
+    if (computerChoice.charAt(i) == keyPress) {
+      allBlanks[i] = keyPress;
+      wordBlanks.textContent = allBlanks.join(" ");
+    } 
+/* Add condition that finds unused wrong keypresses and displays them for user  
+and takes away a life */  
+    else if (
+      wrongGuess.indexOf(keyPress) == -1 &&
+      computerChoice.indexOf(keyPress) == -1
+    ) {
+      wrongGuess.push(keyPress);
+      lives--;
+      document.getElementById("lives").innerHTML = lives;
+      wrongGuess.sort();
+      document.getElementById("guessed-letters").innerHTML = wrongGuess.join(" ");
+    }
+  }
 };
 
-
-
-
-
-// alphabet.splice(userGuess, userGuess);
-
-
-// document.getElementById("guessed-letters").innerHTML = alphabet;
-// console.log(alphabet);
