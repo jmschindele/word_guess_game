761900eb25872287f2d53aa01392e814db537c58
jmschindele <45886434+jmschindele@users.noreply.github.com>
2019-03-01 23:32:45 -0500

    fixed javascript to include win talley and reset words. Changed win/lose conditions to onkeyup for better functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 // create an array of words for the computer to guess
-var targetWord = ["apple", "banana", "orange", "grape", "strawberry"];
+var targetWord = ["apple", "banana", "orange", "grape", "strawberry","cranberry","papaya","dragonfruit"];
 // define other variables
 var start;
 var wordIndex = 0;
@@ -11,20 +11,29 @@ var keyPress;
 var userInput = document.getElementById("user-input");
 var allBlanks = [];
 var wrongGuess = [];
-var wins = 0;
+var wins;
+var lives;
 
 //Set lives to 6 and add to page
-var lives = 6;
+lives = 6;
 document.getElementById("lives").innerHTML = lives;
-// computer guesses a word
 
+//Set wins to 0 on load up
+
+wins = 0;
+document.getElementById("wins").innerHTML = wins;
+
+// Capture starting function to allow multiple plays without reload
+
+// computer guesses a word
 function wordGen() {
   wordIndex = Math.floor(Math.random() * targetWord.length);
 }
+
 wordGen();
 computerChoice = targetWord[wordIndex];
 
-// computer converts word into underscores and displays on wedpage
+// computer converts word into underscores and displays on webpage
 
 var wordBlanks = document.createElement("span");
 
@@ -35,34 +44,26 @@ for (var i = 0; i < computerChoice.length; i++) {
 }
 
 // user inputs a key to guess word
-document.onkeypress = function(e) {
+document.onkeydown = function(e) {
   keyPress = event.key;
 
- //Add condition that prompts user of repeated keys 
+//remove special characters from inputs
+if (event.keyCode >= 65 && event.keyCode <= 90) {
+
+  //Add condition that prompts user of repeated keys
   if (wrongGuess.indexOf(keyPress) > -1) {
     alert("You've already pressed that letter!");
   }
 
-//Add win condition
-  if (computerChoice == allBlanks.join("").toString()) {
-    alert("You Win!");
-    location.reload();
-    win++;
-  }
-//Add gameover condition
-  if (lives === 0) {
-    alert("You Lose!");
-    location.reload();
-  }
-//Add a for loop to replace blanks with characters based on position
+
+  //Add a for loop to replace blanks with characters based on position
   for (var i = 0; i < computerChoice.length; i++) {
-    if (computerChoice.charAt(i) == keyPress) {
+    if (computerChoice.charAt(i) == keyPress){
       allBlanks[i] = keyPress;
       wordBlanks.textContent = allBlanks.join(" ");
-    } 
-/* Add condition that finds unused wrong keypresses and displays them for user  
-and takes away a life */  
-    else if (
+    } else if (
+    /* Add condition that finds unused wrong keypresses and displays them for user  
+and takes away a life */
       wrongGuess.indexOf(keyPress) == -1 &&
       computerChoice.indexOf(keyPress) == -1
     ) {
@@ -70,8 +71,39 @@ and takes away a life */
       lives--;
       document.getElementById("lives").innerHTML = lives;
       wrongGuess.sort();
-      document.getElementById("guessed-letters").innerHTML = wrongGuess.join(" ");
+      document.getElementById("guessed-letters").innerHTML = wrongGuess.join(
+        " "
+      );
     }
   }
+}
 };
 
+// place win and lose conditions on Key Up for timing consistency
+document.onkeyup = function(e) {
+    //Add win condition
+    if (computerChoice == allBlanks.join("").toString()) {
+      alert("You Win!");
+      wins++;
+      document.getElementById("wins").innerHTML = wins;
+    //Choose new word
+      wordGen();
+      computerChoice = targetWord[wordIndex];
+    //Reset Stats
+      lives = 7;
+      allBlanks = [];
+      wrongGuess = [];
+    // computer converts word into underscores and displays on webpage
+      for (var i = 0; i < computerChoice.length; i++) {
+          allBlanks.push("_");
+          wordBlanks.textContent = allBlanks.join(" ");
+          document.getElementById("unsolved-puzzle").appendChild(wordBlanks);
+          }
+    }
+
+      //Add gameover condition
+  if (lives === 0) {
+    alert("You Lose!");
+    location.reload();
+  }
+} 
